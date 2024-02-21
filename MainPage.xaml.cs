84a990e0e46566beb747bae967f02ee337beae9d
Ruben Darío Rueda <ingeniero.rubenrueda@gmail.com>
2024-02-21 07:52:42 -0500

    FirstCommit
    Excercises with implementations of How pass arguments using XAML

---
+++ MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace PasarArgumentos
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        //Color color = new Color(3,5);
+        
+    }
+
+}
